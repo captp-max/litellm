dac63aa48f7637a3fe93ca95192281575a4adc8a
captp-max <alexis.orgueil@gmail.com>
2026-05-16 16:36:42 +0200

    Update remove_name.py

--- proxy/hooks/remove_name.py
+++ proxy/hooks/remove_name.py
@@ -1,10 +1,13 @@
 from litellm.integrations.custom_logger import CustomLogger
 
 class RemoveNameHook(CustomLogger):
-    def async_pre_call_hook(self, user_api_key_dict, cache_hit, data, call_type):
-        """Supprime le champ 'name' de tous les messages avant envoi"""
-        if isinstance(data, dict) and "messages" in data:
+    async def async_pre_call_hook(self, user_api_key_dict, cache_hit, data, call_type):
+        """Supprime tous les champs 'name' des messages"""
+        if isinstance(data, dict) and "messages" in data and isinstance(data["messages"], list):
             for message in data["messages"]:
-                if isinstance(message, dict) and "name" in message:
-                    message.pop("name", None)   # supprime proprement
+                if isinstance(message, dict):
+                    message.pop("name", None)  # Supprime si présent
         return data
+
+# Instance à utiliser dans le config
+remove_name_hook = RemoveNameHook()
